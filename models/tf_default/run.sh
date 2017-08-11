b6f70820026d4c3526368a499bf59dc29005f9a6
Gregory Pasternak <griffonn@gmail.com>
2017-08-11 19:21:28 +0300

    add run.sh

---
+++ models/tf_default/run.sh
@@ -0,0 +1,1 @@
+python3 word2vec_dlce.py --train_data=/cs/engproj/3deception/meaning/models_data/text1/text1 --eval_data=../../data/test-antonyms.txt --save_path=/tmp/text1t --syn_threshold=10 --ant_threshold=2
