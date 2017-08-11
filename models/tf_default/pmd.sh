01d0eea26fca228fa074d0c189690c750014745d
Gregory Pasternak <griffonn@gmail.com>
2017-08-11 16:08:42 +0300

    add pond source file

---
+++ models/tf_default/pmd.sh
@@ -0,0 +1,3 @@
+source /cs/engproj/3deception/meaning/l2venv/bin/activate.csh
+setenv LD_LIBRARY_PATH /usr/local/nvidia/cuda/8.0/lib64:/usr/local/nvidia/375.39/lib:/usr/local/nvidia/cuda/cudnn/cudnn-8.0-linux-x64/lib64/
+
